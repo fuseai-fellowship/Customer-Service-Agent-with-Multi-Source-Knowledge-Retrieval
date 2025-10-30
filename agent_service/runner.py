16ee1f4f0f241f38fc86da585f9793c44ae9d844
Sweta <swetasuwal001@gmail.com>
2025-10-30 20:04:45 +0545

    fix runner

--- agent_service/runner.py
+++ agent_service/runner.py
@@ -15,7 +15,7 @@ def code_runner(user_name:str, user_input:str, chat_history:str):
         # fresh state for this run
     state = {
         "messages": [],
-        "summary": chat_history,  # inject prior conversation
+        "summary": f"Chat History:{chat_history}\n",  # inject prior conversation
         "tool_output": "",
         "review_decision": ReviewDecision(decision="needs_more"),  # initial placeholder
         "user_name": user_name
@@ -24,7 +24,7 @@ def code_runner(user_name:str, user_input:str, chat_history:str):
     # add current user message
     human_msg = HumanMessage(content=user_input)
     state["messages"].append(human_msg)
-    # state["summary"] += f"\nHuman: {user_input}"
+    state["summary"] += f"\nCurrent User Query: {user_input}"
 
     # invoke graph
     result = graph.invoke(state)
@@ -33,6 +33,6 @@ def code_runner(user_name:str, user_input:str, chat_history:str):
     review = result.get("review_decision")
     final_answer = review.answer if review else "(no answer)"
 
-    # chat_summary = f"\nHuman: {user_input}\nAI: {final_answer}"
-    chat_summary = result.get("summary", chat_history)
+    chat_summary = f"\nHuman: {user_input}\nAI: {final_answer}"
+    # chat_summary = result.get("summary", chat_history)
     return final_answer, chat_summary
